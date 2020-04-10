78ae2613825fc3ca071baee2507f443ca0ba2e00
Dilmurod <dilmir95@yahoo.com>
2020-04-09 22:16:07 -0500

    driver

--- src/test/java/com/bitrix24/utilities/Driver.java
+++ src/test/java/com/bitrix24/utilities/Driver.java
@@ -1,5 +1,6 @@
-package com.automation.utilities;
+package com.bitrix24.utilities;
 
+import com.bitrix24.utilities.ConfigurationReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
